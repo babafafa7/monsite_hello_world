f6484adc98e6f99bf2e54edb0b00b8a5b325ba79
babafafa7 <bastien.falchero@outlook.fr>
2021-02-10 15:53:52 +0100

    ok

--- anim.js
+++ anim.js
@@ -1,22 +1,24 @@
 function init(){
-    document.getElementById("boutton").addEventListener("mouseover", enSavoirPlus);
-    document.getElementById("boutton").addEventListener("mouseout", enSavoirMoins);
+    document.getElementById("card1").addEventListener("mouseenter", enSavoirPlus);
 }
 
 function enSavoirPlus(){
+    document.getElementById("card1").removeEventListener("mouseenter", enSavoirPlus);
     codeHtml = '<div class="card-body">'
-    +'<p class="card-text">Le texte pour en savoir plus.fzb ekrnnefv erno niaoena banba bann abs s f r rffsfsf sg g z  z z z gzgzgzgzg  zz tz tab tgrg ertg etg ertgertge rtgertgertg etge tgetgerge tg tt ge egerg ggg ebbnb jab obai bb  ba o efsqq qrg qrg qegr qgqr gq</p>'
+    +'<p class="card-text">Tetris est un jeu vidéo de puzzle conçu par Alekseï Pajitnov à partir de juin 1984 sur Elektronika 60. Lors de la création du concept, Pajitnov est aidé de Dmitri Pavlovski et Vadim Guerassimov pour le développement.</p>'
     +'<a href="./tetris/tetris.html" class="btn btn-primary">Jouer</a>';
     document.getElementById("card1").innerHTML = codeHtml;
+    document.getElementById("card1").addEventListener("mouseleave", enSavoirMoins);
 }
 
 function enSavoirMoins(){
+    document.getElementById("card1").addEventListener("mouseleave", enSavoirMoins);
     codeHtml = '<img class="card-img-top" src="./images/H2x1_NSwitchDS_Tetris99.png" alt="tetris">'
     +'<div class="card-body">'
     +'<h5 class="card-title">Tetris</h5>'
-    +'<p class="card-text">Le plus célèbre des jeux de briques.</p>'
-    +'<a href="./tetris/tetris.html" class="btn btn-primary">Jouer</a>'
+    +'<p class="card-text">Le plus célèbre des jeux de briques.</p>';
     document.getElementById("card1").innerHTML = codeHtml;
+    document.getElementById("card1").addEventListener("mouseenter", enSavoirPlus);
 }
 
 window.addEventListener("load", init);
